30276d480199bde7e6493a7cd38264bb82d1f0e8
Dima Davydov <Kilo6559@gmail.com>
2025-03-25 13:28:18 +0200

    Start

---
+++ src/main/java/Car_sharing_app/com/CarGoSharingAppApplication.java
@@ -0,0 +1,13 @@
+package Car_sharing_app.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarGoSharingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarGoSharingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Car_sharing_app/com/CarGoSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Car_sharing_app.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarGoSharingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
