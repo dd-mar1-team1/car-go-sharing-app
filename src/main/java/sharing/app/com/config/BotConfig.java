282bdd2e8a5ac1d5cb058b332bc99de57744825f
Dima Davydov <Kilo6559@gmail.com>
2025-04-17 15:14:45 +0300

    Fixed

--- src/main/java/sharing/app/com/config/BotConfig.java
+++ src/main/java/sharing/app/com/config/BotConfig.java
@@ -3,8 +3,10 @@ package sharing.app.com.config;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Data
+@PropertySource("classpath:application-secret.properties")
 @Configuration
 public class BotConfig {
     @Value("${bot.name}")
